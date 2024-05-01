3ed524a1fd9f6118bc0558ca47012bcd692a83d7
JonasPTFL <js.pp@gmx.de>
2024-05-01 21:13:57 +0200

    fix wrong path in constants

--- src/util/constants.py
+++ src/util/constants.py
@@ -1,6 +1,6 @@
 chosen_classes_path = '../../chosen_classes.txt'
 all_available_classes_path = '../../dataset/food-101/meta/classes.txt'
-all_images_classpath = '../dataset/food-101/images/'
+all_images_classpath = '../../dataset/food-101/images/'
 amount_classes = 30
 
 # meta data file paths
