6cb99723dfcb752c1d53222b7a51c52b44befb19
JonasPTFL <js.pp@gmx.de>
2024-05-01 13:32:35 +0200

    refactor image utility functions to utility file - store removed images in same format as meta file images

--- preprocessing/deletion_image_files.py
+++ preprocessing/deletion_image_files.py
@@ -1,6 +1,7 @@
 import constants
 import os
 import numpy as np
+from data_utility import get_image_path
 
 removed_images = set(np.loadtxt(constants.REMOVED_IMAGES_STORAGE_PATH, dtype=str))
 
@@ -9,7 +10,14 @@ if input(f"Are you sure you want to delete {len(removed_images)} images? (yes/no
     # delete image files marked as removed
     for removed_image in removed_images:
         try:
-            os.remove(removed_image)
+            # extract class name and image id from format 'class/id'
+            removed_image_split = removed_image.split('/')
+            class_name = removed_image_split[0]
+            image_id = removed_image_split[1]
+            # get image path and delete the image
+            image_path = get_image_path(class_name, image_id)
+            os.remove(image_path)
+            print(f'Deleted {removed_image}')
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (removed_image, e))
     print('Deletion operation completed.')
--- preprocessing/remove_image_detection.py
+++ preprocessing/remove_image_detection.py
@@ -3,30 +3,7 @@ import shutil
 
 import numpy as np
 import constants
-
-
-def get_image_path(class_name, image_id):
-    """
-    Creates the path to the image using the class name and the image id. Uses constant jpg as file extension since
-    all images of the food-101 dataset are in jpg format.
-    :param class_name: name of the class
-    :param image_id: id of the image
-    :return: project relative path to the image
-    """
-    # join path to dataset images with the class and id
-    # ensure that the path uses forward slashes as join produces backslashes on windows
-    return os.path.join(constants.all_images_classpath, class_name, f"{image_id}.jpg").replace("\\", "/")
-
-
-def get_image_path_from_meta_data(metadata):
-    """
-    Extracts the class name and image id from the metadata and returns the path to the image.
-    This metadata format is used in the meta files of the food-101 dataset.
-    :param metadata: metadata of the image in the format 'class/id'
-    :return: project relative path to the image
-    """
-    class_name, image_id = metadata.split("/")
-    return get_image_path(class_name, image_id)
+from preprocessing.data_utility import get_image_path_from_meta_data, get_image_path
 
 
 def get_all_chosen_image_paths():
@@ -85,8 +62,12 @@ if removed_images_count > 0:
     if confirmation_input == confirmation_keyword:
         # append removed images to file
         with open(constants.REMOVED_IMAGES_STORAGE_PATH, 'a') as removed_images_file:
+            # sort removed images alphabetically
+            removed_images = sorted(removed_images)
             for removed_image in removed_images:
-                removed_images_file.write(f"{removed_image}\n")
+                class_name = os.path.basename(os.path.dirname(removed_image))
+                image_id = os.path.basename(removed_image).split('.')[0]
+                removed_images_file.write(f"{class_name}/{image_id}\n")
     elif confirmation_input == display_keyword:
         # show removed images
         for removed_image in removed_images:
