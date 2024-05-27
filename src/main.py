25b0f5cae0fe98c39eeac4c366b9f17c004a2a70
Julius Emil Arendt <98806292+Aremju@users.noreply.github.com>
2024-05-27 14:22:22 +0200

    update main.py
    * added last architecture

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ model_iterations = [
             metrics=["accuracy"]
         ),
         iteration_name='stoic_axolotl_6_2',
-        epochs=10,
+        epochs=40,
         allowed_to_run=False
     ),
     ModelIteration(
@@ -68,8 +68,8 @@ model_iterations = [
             metrics=["accuracy"]
         ),
         iteration_name='calm_alpaca_7_3',
-        epochs=30,
-        allowed_to_run=False
+        epochs=40,
+        allowed_to_run=True
     ),
     ModelIteration(
         model_architecture=ModelArchitecture(
@@ -100,8 +100,8 @@ model_iterations = [
             metrics=["accuracy"]
         ),
         iteration_name='sweet_hedgehog_7_3_bn',
-        epochs=10,
-        allowed_to_run=False
+        epochs=40,
+        allowed_to_run=True
     ),
     ModelIteration(
         model_architecture=ModelArchitecture(
@@ -135,8 +135,8 @@ model_iterations = [
             metrics=["accuracy"]
         ),
         iteration_name='angry_dalmatian_7_3_bn',
-        epochs=20,
-        allowed_to_run=False
+        epochs=40,
+        allowed_to_run=True
     ),
     ModelIteration(
         model_architecture=ModelArchitecture(
@@ -167,13 +167,13 @@ model_iterations = [
                     tf.keras.layers.Dense(constants.NUM_CLASSES, activation='softmax')
                 ]
             ),
-            optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+            optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
             metrics=["accuracy"]
         ),
         iteration_name='friendly_elf_10_2_bn_do',
-        epochs=20,
-        allowed_to_run=False
+        epochs=40,
+        allowed_to_run=True
     ),
     ModelIteration(
         model_architecture=ModelArchitecture(
@@ -207,8 +207,8 @@ model_iterations = [
             metrics=["accuracy"]
         ),
         iteration_name='hungry_chicken_10_2_bn_do',
-        epochs=30,
-        allowed_to_run=False
+        epochs=40,
+        allowed_to_run=True
     ),
     ModelIteration(
         model_architecture=ModelArchitecture(
@@ -237,7 +237,7 @@ model_iterations = [
         ),
         iteration_name='arrogant_dwarf_6_2_l2',
         epochs=40,
-        allowed_to_run=False
+        allowed_to_run=True
     ),
     ModelIteration(
         model_architecture=ModelArchitecture(
@@ -266,7 +266,7 @@ model_iterations = [
         ),
         iteration_name='sporty_kangaroo_6_2_l2',
         epochs=40,
-        allowed_to_run=False
+        allowed_to_run=True
     ),
     ModelIteration(
         model_architecture=ModelArchitecture(
@@ -297,11 +297,44 @@ model_iterations = [
             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
             metrics=["accuracy"]
         ),
-        iteration_name='big_elefant_6_2_l2',
-        epochs=3,
+        iteration_name='big_elefant_10_2_l2',
+        epochs=40,
+        allowed_to_run=True
+    ),
+    ModelIteration(
+        model_architecture=ModelArchitecture(
+            architecture=tf.keras.Sequential(
+                [
+                    tf.keras.layers.Rescaling(1. / 255),
+                    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+                    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+                    tf.keras.layers.MaxPooling2D(),
+                    tf.keras.layers.Conv2D(64, 3, activation='relu'),
+                    tf.keras.layers.Conv2D(64, 3, activation='relu'),
+                    tf.keras.layers.MaxPooling2D(),
+                    tf.keras.layers.Conv2D(128, 3, activation='relu'),
+                    tf.keras.layers.Conv2D(128, 3, activation='relu'),
+                    tf.keras.layers.MaxPooling2D(),
+                    tf.keras.layers.Conv2D(256, 3, activation='relu'),
+                    tf.keras.layers.Conv2D(256, 3, activation='relu'),
+                    tf.keras.layers.MaxPooling2D(),
+                    tf.keras.layers.Conv2D(512, 3, activation='relu'),
+                    tf.keras.layers.Conv2D(512, 3, activation='relu'),
+                    tf.keras.layers.MaxPooling2D(),
+                    tf.keras.layers.Flatten(),
+                    tf.keras.layers.Dense(4096, activation='relu', kernel_regularizer='l2'),
+                    tf.keras.layers.Dense(4096, activation='relu', kernel_regularizer='l2'),
+                    tf.keras.layers.Dense(constants.NUM_CLASSES, activation='softmax')
+                ]
+            ),
+            optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=["accuracy"]
+        ),
+        iteration_name='annoying_tortoise_10_3_l2',
+        epochs=40,
         allowed_to_run=True
     )
-
 ]
 
 if __name__ == '__main__':
@@ -310,4 +343,3 @@ if __name__ == '__main__':
     for model_iteration in model_iterations:
         if model_iteration.is_allowed_to_run():
             model_iteration.run()
-
