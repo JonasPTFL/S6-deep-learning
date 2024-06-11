802958b14d20fb2c15ac6364c1a4b33ced0217bb
Julius Emil Arendt <98806292+Aremju@users.noreply.github.com>
2024-06-11 10:18:25 +0200

    Update history.py

--- src/evaluation/metrics/history.py
+++ src/evaluation/metrics/history.py
@@ -18,7 +18,7 @@ def plot_loss_values(model_history, model_id, timestamp):
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Validation'], loc='upper left')
     plt.savefig(f"../reports/{model_id}/{timestamp}/train_val_loss.png")
-    plt.show()
+    plt.clf()
 
 
 def plot_accuracy_values(model_history, model_id, timestamp):
@@ -34,13 +34,15 @@ def plot_accuracy_values(model_history, model_id, timestamp):
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Validation'], loc='upper left')
     plt.savefig(f"../reports/{model_id}/{timestamp}/train_val_accuracy.png")
-    plt.show()
+    plt.clf()
 
 
 class History(AbstractMetric, ABC):
     def calculate_metric(self, model: tf.keras.Model = None,
                          test_dataset: tf.data.Dataset = None,
+                         train_dataset: tf.data.Dataset = None,
                          model_id: str = "-1", model_history=None,
                          model_timestamp=None):
         plot_accuracy_values(model_history, model_id, model_timestamp)
         plot_loss_values(model_history, model_id, model_timestamp)
+        plt.clf()
